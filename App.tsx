ed2ccc346ee2f9a3ab73ec9226f5f6936b496439
yudianwebdev <69705828+yudianwebdev@users.noreply.github.com>
2021-02-16 11:56:21 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import Routes from './src/routes';
+import Routes from './src/routes s';
 
 const App = () => {
   return (
